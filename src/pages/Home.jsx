3c54d3046a5df4a8100fd30d1330d34f8131f1ef
Priyanshu Agrahari <priyanshuagrahari@Priyanshus-MacBook-Pro-2.local>
2025-05-08 17:03:38 +0530

    Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,21 +45,22 @@ const Home = () => {
       <section className="hero-section">
         <div className="hero-overlay">
           <h1 className='main'>Discover the Finest Indian Cuisine</h1>
-          <p className='para'>From flavorful curries to aromatic biryanis, find the perfect Indian restaurant for any occasion.</p>
           <div className="search-bar">
             <input type="text" placeholder="Enter location..." className='location'/>
             <select className='cuisine'>
               <option>All Cuisines</option>
-              <option>French</option>
-              <option>Greek</option>
-              <option>Nordic</option>
+              <option>North-Indian</option>
+              <option>Indian</option>
+              <option>Italian</option>
+              <option>Japanese</option>
+              <option>Chinese</option>
             </select>
             <button>Find Restaurants</button>
           </div>
 
         </div>
       </section>
-      
+
       <section className="featured-section">
         <div className="featured-header">
           <h2>Featured Restaurants</h2>
